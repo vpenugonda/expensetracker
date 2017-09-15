f13875f808050f482cdaa1f6253e4e20a86d318a
Siddhartha <vpenugonda@student.fairfield.edu>
2017-09-15 14:32:04 -0400

    Automatic Update.

--- index.php
+++ index.php
@@ -87,7 +87,7 @@ if (!empty($_GET['del_id'])) {
 <div class="container">
     <div class="row">
         <div class="header clearfix">
-            <nav>
+            <nav> 
                 <ul class="nav nav-pills float-right">
                     <li class="nav-item">
                         <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
